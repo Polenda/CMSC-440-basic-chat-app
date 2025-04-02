47bafb4fd964de21657d8c992b1720398ee52816
Daniel <daniel.a.polen@gmail.com>
2025-04-02 19:51:59 -0400

    pre comments

--- ChatClient.py
+++ ChatClient.py
@@ -4,111 +4,114 @@ import json
 import sys
 from datetime import datetime
 
-class Client:
-    def __init__(self, server_ip, server_port, nickname, client_id):
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.nickname = nickname
-        self.client_id = client_id
-        self.start_time = get_time()
-        self.sent = 0
-        self.received = 0
-        self.char_sent = 0
-        self.char_recv = 0
+msg_sent = 0
+msg_rcv = 0
+char_sent = 0
+char_rcv = 0
+start_time = ""
+nickname = ""
+client_id = ""
+client_sock = None
 
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((self.server_ip, self.server_port))
-        except:
-            print("ERR - Unable to connect to server.")
-            sys.exit(1)
-
-        print(f"ChatClient started with server IP: {self.server_ip}, port: {self.server_port}, nickname: {self.nickname}, client ID: {self.client_id}, Date/Time: {self.start_time}")
-        self.send_initial()
-
-        threading.Thread(target=self.receive_messages, daemon=True).start()
-        self.send_loop()
-
-    def send_initial(self):
-        initial_msg = {
-            "type": "nickname",
-            "nickname": self.nickname,
-            "clientID": self.client_id,
-            "timestamp": self.start_time
-        }
-        self.sock.send(json.dumps(initial_msg).encode())
-
-    def receive_messages(self):
-        while True:
-            try:
-                msg = self.sock.recv(1024).decode()
-                if not msg:
-                    break
-                data = json.loads(msg)
-
-                if data["type"] == "error":
-                    print(data["message"])
-                    self.sock.close()
-                    sys.exit(1)
-
-                if data["type"] == "broadcast":
-                    timestamp = data["timestamp"]
-                    nickname = data["nickname"]
-                    message = data["message"]
-                    print(f"{timestamp} :: {nickname}: {message}")
-                    self.received += 1
-                    self.char_recv += len(message)
-
-            except:
-                break
-
-    def send_loop(self):
-        while True:
-            msg = input()
-            if msg.strip().lower() == "disconnect":
-                self.sock.send(json.dumps({
-                    "type": "disconnect",
-                    "nickname": self.nickname,
-                    "clientID": self.client_id
-                }).encode())
-                break
-
-            ts = get_time()
-            data = {
-                "type": "message",
-                "nickname": self.nickname,
-                "message": msg,
-                "timestamp": ts
-            }
-            self.sock.send(json.dumps(data).encode())
-            self.sent += 1
-            self.char_sent += len(msg)
-
-        end_time = get_time()
-        print(f"Summary: start: {self.start_time}, end: {end_time}, msg sent:{self.sent}, msg rcv:{self.received}, char sent:{self.char_sent}, char rcv:{self.char_recv}")
-        self.sock.close()
-
-def get_time():
+def get_time() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-if __name__ == '__main__':
+def receive():
+    global msg_rcv, char_rcv
+    while True:
+        try:
+            data = client_sock.recv(1024).decode()
+            if not data:
+                break
+            msg = json.loads(data)
+
+            if msg["type"] == "error":
+                print(msg["message"])
+                disconnect()
+                sys.exit(1)
+
+            elif msg["type"] == "broadcast":
+                print(f"{msg['timestamp']} :: {msg['nickname']}: {msg['message']}")
+                msg_rcv += 1
+                char_rcv += len(msg["message"])
+        except:
+            break
+
+def send():
+    global msg_sent, char_sent
+    try:
+        while True:
+            message = input()
+            if message.strip().lower() == "disconnect":
+                disconnect()
+                break
+            timestamp = get_time()
+            chat_msg = {
+                "type": "message",
+                "nickname": nickname,
+                "message": message,
+                "timestamp": timestamp
+            }
+            client_sock.send(json.dumps(chat_msg).encode())
+            msg_sent += 1
+            char_sent += len(message)
+    except KeyboardInterrupt:
+        disconnect()
+
+def disconnect():
+    global client_sock
+    dis_msg = {
+        "type": "disconnect",
+        "nickname": nickname,
+        "clientID": client_id
+    }
+    try:
+        client_sock.send(json.dumps(dis_msg).encode())
+    except:
+        pass
+    end_time = get_time()
+    print(f"\nSummary: start: {start_time}, end: {end_time}, msg sent:{msg_sent}, msg rcv:{msg_rcv}, char sent:{char_sent}, char rcv:{char_rcv}")
+    client_sock.close()
+    sys.exit(0)
+
+def main():
+    global nickname, client_id, client_sock, start_time
+
     if len(sys.argv) != 5:
-        print(f"ERR - arg {len(sys.argv)}")
+        print("ERR - arg 1" if len(sys.argv) < 2 else f"ERR - arg {len(sys.argv)}")
         sys.exit(1)
 
     host = sys.argv[1]
+    port = int(sys.argv[2]) if sys.argv[2].isdigit() else -1
+    nickname = sys.argv[3]
+    client_id = sys.argv[4]
+
+    if port < 0 or port > 65535:
+        print("ERR - arg 2")
+        sys.exit(1)
+
     try:
         ip = socket.gethostbyname(host)
     except:
         print("ERR - arg 1")
         sys.exit(1)
 
-    port = int(sys.argv[2]) if sys.argv[2].isdigit() and int(sys.argv[2]) < 65536 else -1
-    if port < 0:
-        print("ERR - arg 2")
-        sys.exit(1)
+    start_time = get_time()
+    print(f"ChatClient started with server IP: {ip}, port: {port}, nickname: {nickname}, client ID: {client_id}, Date/Time: {start_time}")
 
-    nickname = sys.argv[3]
-    client_id = sys.argv[4]
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.connect((ip, port))
 
-    Client(ip, port, nickname, client_id)
+    init_msg = {
+        "type": "nickname",
+        "nickname": nickname,
+        "clientID": client_id,
+        "timestamp": start_time
+    }
+    client_sock.send(json.dumps(init_msg).encode())
+
+    threading.Thread(target=receive, daemon=True).start()
+    send()
+
+if __name__ == "__main__":
+    main()
--- ChatServer.py
+++ ChatServer.py
@@ -1,98 +1,125 @@
-import json
 import socket
 import threading
+import json
 import sys
 from datetime import datetime
+import time
 
-class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.clients = {}  # client socket -> {nickname, clientID}
-        self.nicknames = set()
+clients = {}
+nicknames = set()
 
-        try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.bind((self.host, self.port))
-            self.socket.listen()
-            ip = socket.gethostbyname(socket.gethostname())
-            print(f"ChatServer started with server IP: {ip}, port: {self.port} ...")
-        except socket.error:
-            print(f"ERR - cannot create ChatServer socket using port number {self.port}")
-            sys.exit(1)
+def broadcast(message, sender_sock):
+    for client_sock in clients:
+        if client_sock != sender_sock:
+            try:
+                client_sock.send(message)
+            except:
+                client_sock.close()
 
-    def broadcast_message(self, message, sender_sock=None):
-        receivers = []
-        for client_sock in self.clients:
-            if client_sock != sender_sock:
-                try:
-                    client_sock.send(message)
-                    receivers.append(self.clients[client_sock]["nickname"])
-                except:
-                    self.disconnect_client(client_sock)
-        if receivers:
-            print(f"Broadcasted: {', '.join(receivers)}")
+def handle(client_sock, addr):
+    try:
+        init_msg = client_sock.recv(1024).decode()
+        data = json.loads(init_msg)
 
-    def handle_client(self, client_sock, addr):
-        try:
-            data = json.loads(client_sock.recv(1024).decode())
-            if data["type"] != "nickname":
-                raise Exception("Invalid initial message")
-
+        if data["type"] == "nickname":
             nickname = data["nickname"]
-            clientID = data["clientID"]
+            client_id = data["clientID"]
             timestamp = data["timestamp"]
 
-            if nickname in self.nicknames:
-                client_sock.send(json.dumps({ "type": "error", "message": "Nickname is already in use. Please choose a different one." }).encode())
+            if nickname in nicknames:
+                err = { "type": "error", "message": "Nickname is already in use. Please choose a different one." }
+                client_sock.send(json.dumps(err).encode())
                 client_sock.close()
                 return
 
-            self.clients[client_sock] = {"nickname": nickname, "clientID": clientID, "addr": addr}
-            self.nicknames.add(nickname)
+            clients[client_sock] = {
+                "addr": addr,
+                "nickname": nickname,
+                "clientID": client_id
+            }
+            nicknames.add(nickname)
+
             print(f"{timestamp} :: {nickname}: connected.")
-
-            while True:
-                message = client_sock.recv(1024).decode()
-                if not message:
-                    break
-
-                msg_data = json.loads(message)
-                if msg_data["type"] == "disconnect":
-                    break
-                elif msg_data["type"] == "message":
-                    ts = msg_data["timestamp"]
-                    msg = msg_data["message"]
-                    ip, port = addr
-                    size = len(msg)
-                    print(f"Received: IP:{ip}, Port:{port}, Client-Nickname:{nickname}, ClientID:{clientID}, Date/Time:{ts}, Msg-Size:{size}")
-                    self.broadcast_message(message.encode(), client_sock)
-
-        except Exception:
-            pass
-        finally:
-            self.disconnect_client(client_sock)
-
-    def disconnect_client(self, client_sock):
-        if client_sock in self.clients:
-            nickname = self.clients[client_sock]["nickname"]
-            print(f"{get_time()} :: {nickname}: disconnected.")
-            self.nicknames.remove(nickname)
-            del self.clients[client_sock]
+        else:
             client_sock.close()
+            return
 
-def get_time():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        while True:
+            try:
+                msg = client_sock.recv(1024).decode()
+                if not msg:
+                    break
+                data = json.loads(msg)
 
-if __name__ == '__main__':
+                if data["type"] == "disconnect":
+                    break
+                elif data["type"] == "message":
+                    nickname = data["nickname"]
+                    message = data["message"]
+                    timestamp = data["timestamp"]
+                    ip, port = addr
+                    msg_size = len(message)
+
+                    print(f"\nReceived: IP:{ip}, Port:{port}, Client-Nickname:{nickname}, ClientID:{clients[client_sock]['clientID']}, Date/Time:{timestamp}, Msg-Size:{msg_size}")
+
+                    recipients = [info["nickname"] for sock, info in clients.items() if sock != client_sock]
+                    print(f"Broadcasted: {', '.join(recipients)}")
+
+                    broadcast(json.dumps({
+                        "type": "broadcast",
+                        "nickname": nickname,
+                        "message": message,
+                        "timestamp": timestamp
+                    }).encode(), client_sock)
+            except Exception:
+                break
+
+    finally:
+        if client_sock in clients:
+            nickname = clients[client_sock]["nickname"]
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} :: {nickname}: disconnected.")
+            nicknames.remove(nickname)
+            del clients[client_sock]
+        client_sock.close()
+
+def start_server(port, sock):
+    try:
+        sock.settimeout(0.1)
+        while True:
+            try:
+                client_sock, addr = sock.accept()
+                thread = threading.Thread(target=handle, args=(client_sock, addr))
+                thread.start()
+            except socket.timeout:
+                continue
+    except KeyboardInterrupt:
+        print("\nServer shutting down gracefully...")
+    finally:
+        sock.close()
+        for client_sock in list(clients):
+            client_sock.close()
+        sys.exit(0)
+
+def main():
+    port = int(sys.argv[1])
+    try:
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.bind(('', port))
+        server_sock.listen()
+        ip = socket.gethostbyname(socket.gethostname())
+        print(f"ChatServer started with server IP: {ip}, port: {port} ...")
+    except:
+        print(f"ERR - cannot create ChatServer socket using port number {port}")
+        sys.exit(1)
+
+    for i in range(2):
+        time.sleep(1)
+        print(".")
+    
+    start_server(port, server_sock)
+
+if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) > 65535:
         print("ERR - arg 1")
         sys.exit(1)
-
-    port = int(sys.argv[1])
-    host = '0.0.0.0'
-    server = Server(host, port)
-
-    while True:
-        client_sock, addr = server.socket.accept()
-        threading.Thread(target=server.handle_client, args=(client_sock, addr)).start()
+    main()
